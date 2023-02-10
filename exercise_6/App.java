59c6c62123752a4272062e04ce1147406484c855
Mark Joshua <34602758+kaneshii@users.noreply.github.com>
2023-02-10 11:40:33 +0800

    Create App.java

---
+++ exercise_6/App.java
@@ -0,0 +1,43 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+      GroceryItem item1 = new GroceryItem();
+
+      item1.name = "Tissue";
+
+      item1.showItemName();
+
+      GroceryItem item2 = new Toothbrush();
+      item2.name = "colgate ";
+      item2.showItemName();
+      item2.price = 100;
+      
+      
+
+      GroceryItem item3 = new Toothpaste();
+      item3.name = "sensodyne ";
+      item3.price = 50;
+      item3.showItemName();
+    
+      Cashier c1 = new Cashier();
+
+      c1.checkOut(item2);
+      c1.checkOut(item3);
+      c1.showItemPrice(item2);
+      c1.showItemPrice(item3);
+
+      GroceryItem[] itemArray = new GroceryItem[2];
+      itemArray[0] = item2;
+      itemArray[1] = item3;
+
+      for (int i = 0; i <itemArray.length; i++) {
+            itemArray[i].showItemName();
+      }
+
+      Dog myDog = new Dog();
+      myDog.eat();
+
+      Lion myLion = new Lion();
+      myLion.run();
+    }
+}
